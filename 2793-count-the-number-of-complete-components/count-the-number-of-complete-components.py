86168beb4bc2a41cf77b9d3132aa7a9d4aab1693
Ciranjeevi-Dashnamoorthy <ciranjeevi.dashnamoorthy@gmail.com>
2026-07-11 11:54:42 +0530

    Time: 32 ms (81.68%) | Memory: 19.8 MB (71.89%) - LeetSync

---
+++ 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.py
@@ -0,0 +1,60 @@
+class Solution:
+    def countCompleteComponents(self, n: int, edges: List[List[int]]) -> int:
+
+        adj=[[] for _ in range(n)]
+
+        degree=[0]*n
+
+        for u,v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+            degree[u]+=1
+            degree[v]+=1
+
+        
+     
+        ct=0
+        
+        
+        components=[]
+        vis=set()
+    
+        for i in range(n):
+            
+            node=i
+            curr=[]
+            stack=[]
+            
+            if node not in vis:
+                vis.add(node)
+                stack.append(node)
+                curr.append(node)
+
+            while stack:
+                curr_node=stack.pop()
+                for nei in adj[curr_node]:
+                    if nei not in vis:
+                        vis.add(nei)
+                        stack.append(nei)
+                        curr.append(nei)
+            if curr:
+                components.append(curr)
+        
+        print(components)
+        print(degree)
+        
+        for comp in components:
+            
+            target=len(comp)-1
+            fl=True
+            for node in comp:
+
+                if target!=degree[node]:
+                    fl=False
+                    break
+            if fl:
+                ct+=1
+            
+        return ct
+                
+        
