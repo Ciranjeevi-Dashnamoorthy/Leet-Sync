9700911b7c64eb420a6ce5e0881105370f03d9a7
Ciranjeevi-Dashnamoorthy <ciranjeevi.dashnamoorthy@gmail.com>
2026-08-04 20:47:22 +0530

    Time: 612 ms (15.96%) | Memory: 19.5 MB (63.90%) - LeetSync

--- 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.py
+++ 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.py
@@ -1,6 +1,14 @@
 class Solution:
     def findNumberOfLIS(self, nums: List[int]) -> int:
 
+        """
+        Stndard lis with count array
+        c[i] represents the number of LIS ends with ith element
+
+        then how do we initiate the first one and count the already existing ones ??
+
+        """
+
         n=len(nums)
 
         dp=[1]*n
@@ -10,10 +18,13 @@ class Solution:
                 if nums[i]>nums[j] and dp[j]+1>dp[i]:
                     dp[i]=dp[j]+1
                     count[i]=count[j]
+
                 elif nums[j]<nums[i] and dp[j]+1==dp[i]:
                     dp[i]=dp[j]+1
                     count[i]+=count[j]
-
+                    
+               
+      
 
         maxi=max(dp)
         ans=0
