1891397abff35920f7b10e0b604725a1e3157ea3
Ciranjeevi-Dashnamoorthy <ciranjeevi.dashnamoorthy@gmail.com>
2026-08-04 20:24:36 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (18.85%) - LeetSync

--- 4107-find-missing-elements/find-missing-elements.py
+++ 4107-find-missing-elements/find-missing-elements.py
@@ -2,13 +2,13 @@ class Solution:
     def findMissingElements(self, nums: List[int]) -> List[int]:
         """
         When the Random wave of sadness passes and you remember you're actually goated
-        
+
         """
         nums.sort()
         s=set(nums)
         m,l=nums[0],nums[-1]
         li=[]
-        for i in range(m,l+1):
+        for i in range(m+1,l):
             if i not in s:
                 li.append(int(i))
         return li
