2c102998a78ef11629a8ae3a6fbb544511e10865
Ciranjeevi-Dashnamoorthy <ciranjeevi.dashnamoorthy@gmail.com>
2026-08-05 14:17:06 +0530

    Time: 239 ms (84.85%) | Memory: 100.2 MB (78.79%) - LeetSync

---
+++ 3561-remove-methods-from-project/remove-methods-from-project.py
@@ -0,0 +1,48 @@
+class Solution:
+    def remainingMethods(self, n: int, k: int, invocations: List[List[int]]) -> List[int]:
+        
+        adj=[[] for _ in range(n)]
+        from collections import deque
+
+
+        for u,v in invocations:
+
+            adj[u].append(v)
+        
+        queue=deque([k])
+        inf=set()
+        inf.add(k)
+
+        while queue:
+            node=queue.popleft()
+
+            for nei in adj[node]:
+                if nei not in inf:
+                    inf.add(nei)
+                    queue.append(nei)
+
+       
+        for u in range(n):
+            fl=False
+            if u not in inf:
+                for v in adj[u]:
+                    if v in inf:
+                        fl=True
+                        break
+            if fl:
+                break
+        res=[]
+        if fl:
+            return list(range(n))
+        else:
+            for i in range(n):
+                if i not in inf:
+                    res.append(i)
+            return res
+            
+
+
+        
+        
+
+        
