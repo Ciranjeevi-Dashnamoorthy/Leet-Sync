1e377a1b88f0875078098b66c9b0c3dd3808a09d
Ciranjeevi-Dashnamoorthy <ciranjeevi.dashnamoorthy@gmail.com>
2026-08-05 22:32:50 +0530

    Time: 30 ms (24.09%) | Memory: 21.6 MB (12.13%) - LeetSync

---
+++ 87-scramble-string/scramble-string.py
@@ -0,0 +1,28 @@
+class Solution:
+    def isScramble(self, s1: str, s2: str) -> bool:
+        from collections import Counter
+        n=len(s1)
+        
+        @cache
+        def check(s1,s2):
+
+            if s1==s2:
+                return True
+            
+            if len(s1)!=len(s2) or Counter(s1)!=Counter(s2):
+                return False
+            n=len(s1)
+            
+            for i in range(1,n):
+
+                if check(s1[i:],s2[i:]) and check(s1[:i],s2[:i]):
+                    return True
+                
+                if check(s1[:i],s2[-i:]) and check(s1[i:],s2[:-i]):
+                    return True
+            return False
+        
+        return check(s1,s2)
+                
+
+                
