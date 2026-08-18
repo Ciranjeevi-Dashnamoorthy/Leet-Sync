55d93a6c54d9fa293af6b2b916ddab8a80599aa4
Ciranjeevi-Dashnamoorthy <ciranjeevi.dashnamoorthy@gmail.com>
2026-08-18 22:08:17 +0530

    Time: 5 ms (40.57%) | Memory: 19.3 MB (35.66%) - LeetSync

--- 3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.py
+++ 3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.py
@@ -4,28 +4,13 @@ class Solution:
         n=len(nums)
         from collections import Counter
 
-        if k==1:
-            d=Counter(nums)
-            maxi=-1
-            for num in d:
-                if d[num]==1:
-                    maxi=max(maxi,num)
-            return maxi
+       
         if k==n:
             return max(nums)
         
-        # s=set(nums[1:n-1])
-        # if nums[0]!=nums[-1]:
-        #     maxi=max(nums[0],nums[-1])
-        #     if maxi not in s:
-        #         return maxi
-        #     else:
-        #         return -1
-        # else:
-        #     return -1
-        maxi=-1
         from collections import defaultdict
         d=defaultdict(int)
+        maxi=-1
         for i in range(n-k+1):
             for j in range(i,i+k):
                 d[nums[j]]+=1
