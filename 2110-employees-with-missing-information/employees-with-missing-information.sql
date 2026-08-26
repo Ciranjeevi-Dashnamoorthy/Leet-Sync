d61612129626ca3d2ff2df4233436b737eb48e56
Ciranjeevi-Dashnamoorthy <ciranjeevi.dashnamoorthy@gmail.com>
2026-08-26 14:18:19 +0530

    Time: 819 ms (12.23%) | Memory: 0B (100.00%) - LeetSync

--- 2110-employees-with-missing-information/employees-with-missing-information.sql
+++ 2110-employees-with-missing-information/employees-with-missing-information.sql
@@ -1,3 +1,6 @@
+-- Can use COALESCE , but mySQl doesnt support it 
+-- Can do Full Join , also doesnt support
+--  simlate full join using union by simple select or left or right join
 
 select employee_id 
 from Employees 
