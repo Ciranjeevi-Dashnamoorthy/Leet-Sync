2f2499f16ef84c832e3ae1de015061e967d6632b
Ciranjeevi-Dashnamoorthy <ciranjeevi.dashnamoorthy@gmail.com>
2026-08-27 15:46:05 +0530

    Time: 47 ms (6.58%) | Memory: 19.5 MB (42.11%) - LeetSync

--- 4020-lexicographically-smallest-permutation-greater-than-target/lexicographically-smallest-permutation-greater-than-target.py
+++ 4020-lexicographically-smallest-permutation-greater-than-target/lexicographically-smallest-permutation-greater-than-target.py
@@ -4,6 +4,18 @@ class Solution:
         """
         Observation:
 
+        for the current index , check we can place the element which equals target
+        after placing, check can we generate a larger permutation
+
+        if we cant generate or we dont have any charcters to build, then
+        it completely breaks
+        we just find the next greater element which can be placed after 
+        curr in target
+        after placing that one element 
+        build the smallest segment after it 
+
+        if we cant find somehting greater than the current element 
+        return ""
 
         """
         from collections import Counter
