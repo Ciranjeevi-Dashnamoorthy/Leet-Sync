a0ae58603c4507f168250e16c4a7794ce121fce4
Ciranjeevi-Dashnamoorthy <ciranjeevi.dashnamoorthy@gmail.com>
2026-08-29 13:02:19 +0530

    Time: 369 ms (26.12%) | Memory: 47.1 MB (79.10%) - LeetSync

--- 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.py
+++ 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.py
@@ -8,7 +8,6 @@ class Solution:
         res.sort()
         val=[res[0][0]]
         idx=[res[0][1]]
-        # print(res)
         
         for i in range(n-1):
             if res[i+1][0]-res[i][0]<=limit:
@@ -18,28 +17,17 @@ class Solution:
             else:
                 idx.sort()
                 val.sort()
-                # print(idx)
-                # print(val)
+                
                 for j in range(len(val)):
                     nums[idx[j]]=val[j]
                 val=[res[i+1][0]]
                 idx=[res[i+1][1]]
-        #     print(val)
-        #     print(idx)
-        # print("yes")
+        
         idx.sort()
         val.sort()
-        # print(idx)
-        # print(val)
+        
         for i in range(len(val)):
             nums[idx[i]]=val[i]
-        return nums
-
-
-
-
-
-            
         return nums
 
 
