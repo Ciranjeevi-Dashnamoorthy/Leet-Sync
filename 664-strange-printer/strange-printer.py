ec10e16cb9767158cf3237a4e29f420ca2ad4c1a
Ciranjeevi-Dashnamoorthy <ciranjeevi.dashnamoorthy@gmail.com>
2026-09-11 00:18:09 +0530

    Time: 186 ms (64.04%) | Memory: 35.7 MB (14.53%) - LeetSync

--- 664-strange-printer/strange-printer.py
+++ 664-strange-printer/strange-printer.py
@@ -3,20 +3,20 @@ class Solution:
 
         n=len(s)
         @cache
-        def dp(l,r,k):
+        def dp(l,r):
             if l>r:
                 return 0
             
             while l+1<n and s[l]==s[l+1]:
                 l+=1
             
-            ans=1+dp(l+1,r,0)
+            ans=1+dp(l+1,r)
             for m in range(l+1,r+1):
                 if s[l]==s[m]:
-                    ans=min(ans,dp(l+1,m-1,0)+dp(m,r,0))
+                    ans=min(ans,dp(l+1,m-1)+dp(m,r))
             return ans
         
-        return dp(0,n-1,0)
+        return dp(0,n-1)
 
 
         
