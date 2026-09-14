17693bca97634aaf333c8c6868006b88c032445d
Ciranjeevi-Dashnamoorthy <ciranjeevi.dashnamoorthy@gmail.com>
2026-09-14 22:04:55 +0530

    Time: 732 ms (7.07%) | Memory: 84.4 MB (6.06%) - LeetSync

---
+++ 2229-maximum-fruits-harvested-after-at-most-k-steps/maximum-fruits-harvested-after-at-most-k-steps.py
@@ -0,0 +1,56 @@
+class Solution:
+    def maxTotalFruits(self, fruits: List[List[int]], st: int, k: int) -> int:
+
+        n=len(fruits)
+    
+        from collections import defaultdict
+        d=defaultdict(int)
+        for pos,v in fruits:
+            d[pos]=v
+
+        s=max(0,st-k)
+        curr=0
+
+        for i in range(s,st+1):
+            if i in d:
+                curr+=d[i]
+        print(curr)
+        
+        l=st-k
+        steps=k//2
+        ans=curr
+        for r in range(st+1,steps+st+1):
+            
+            
+            curr+=d[r]
+            curr-=d[l]
+            curr-=d[l+1]
+            l+=2
+            ans=max(ans,curr)
+       
+        curr=0
+        for i in range(st,st+k+1):
+            if i in d:
+                curr+=d[i]
+        print("right")
+        print(curr)
+        
+        r=st+k
+        steps=k//2
+        ans=max(ans,curr)
+        s=st-steps
+        
+        for l in range(st-1,s-1,-1):
+            
+            
+            curr+=d[l]
+            curr-=d[r]
+            curr-=d[r-1]
+            r-=2
+            ans=max(ans,curr)
+        print(ans)
+        return ans
+
+
+
+        
