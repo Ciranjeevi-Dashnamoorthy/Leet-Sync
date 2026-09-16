704e6fec8d3d6f7e31a3ca9911f96928ddaa7708
Ciranjeevi-Dashnamoorthy <ciranjeevi.dashnamoorthy@gmail.com>
2026-09-16 22:25:42 +0530

    Time: 2499 ms (17.24%) | Memory: 794.6 MB (5.17%) - LeetSync

---
+++ 1725-number-of-sets-of-k-non-overlapping-line-segments/number-of-sets-of-k-non-overlapping-line-segments.py
@@ -0,0 +1,20 @@
+class Solution:
+    def numberOfSets(self, n: int, k: int) -> int:
+        mod=10**9 + 7
+
+        @cache
+        def dp(idx,seg,fl):
+
+            if seg==0:
+                return 1
+            if idx==n:
+                return 0
+            
+            ans=dp(idx+1,seg,fl)
+            if fl:
+                ans+=dp(idx+1,seg,False)
+            else:
+                ans+=dp(idx,seg-1,True)
+            return ans%mod
+        return dp(0,k,True)
+        
